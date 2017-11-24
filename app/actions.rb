e92543e03d4e7695ff663962e76ca31e8267f1b9
Jay Deo <jaydeo3@gmail.com>
2017-11-24 03:46:51 +0000

    forms all

--- app/actions.rb
+++ app/actions.rb
@@ -4,3 +4,27 @@ get '/' do
 end
 
 
+get '/signup' do        #if a user navitages to the path "/signup",
+    @user = User.new    # setup empty @user object
+    erb(:signup)        # render "app/views/signup/erb"
+end
+
+post '/signup' do
+    
+    # grab user input values from params
+    email       =params[:email]
+    avatar_url  =params[:avatar_url]
+    username    =params[:username]
+    password    =params[:password]
+
+   # instantiate a User
+   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    # if user validations pass and user is saved
+    if @user.save
+        "User #{username} saved"
+ 
+    else
+        erb(:signup)
+    end
+end
