f61d4aa0a2ea15d61c55a5d818be354d069c7d7a
Jay Deo <jaydeo3@gmail.com>
2017-11-29 04:23:01 +0000

    sessions all

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,13 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
 end
-
 
 get '/signup' do        #if a user navitages to the path "/signup",
     @user = User.new    # setup empty @user object
@@ -22,9 +27,34 @@ post '/signup' do
     
     # if user validations pass and user is saved
     if @user.save
-        "User #{username} saved"
- 
+        redirect to('/login')
     else
         erb(:signup)
     end
+end
+
+get '/login' do         #when a GET requests comes into /login
+    erb(:login)
+end
+
+post '/login' do         # when we submit a form with an action of /login
+    username = params[:username]
+    password = params[:password]
+    
+    # 1. find user by username
+    user = User.find_by(username: username)
+    
+    # 2. if that user exists
+    if user && user.password == password
+       session[:user_id] = user.id 
+       redirect to('/')
+            "Success! User with id #{session[:user_id]} is logged in"
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+end
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
 end
